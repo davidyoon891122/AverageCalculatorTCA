3b45011ed5d791449f1aa030c68072db83a5bb95
David Yoon <qqpo12@naver.com>
2024-08-17 10:14:29 +0900

    Initial Commit

---
+++ AveragePriceCalculator/AveragePriceCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  AveragePriceCalculatorApp.swift
+//  AveragePriceCalculator
+//
+//  Created by Jiwon Yoon on 8/17/24.
+//
+
+import SwiftUI
+
+@main
+struct AveragePriceCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
