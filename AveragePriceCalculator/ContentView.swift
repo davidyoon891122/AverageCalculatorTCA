315239bbbce1d6beb57cdb39991f3005ad69fbff
David Yoon <davidyoon@hanwha.com>
2024-11-20 08:20:13 +0900

    Add a WithPerceptionTracking
    * Added a WithPerceptionTracking on the ContentView

--- AveragePriceCalculator/ContentView.swift
+++ AveragePriceCalculator/ContentView.swift
@@ -13,20 +13,22 @@ struct ContentView: View {
     let store: StoreOf<AppFeature>
 
     var body: some View {
-        TabView {
-            DisplayListView(store: store.scope(state: \.tab1, action: \.tab1))
-                .tabItem {
-                    Image(systemName: "list.bullet")
-                    Text("Display")
-                }
-
-            SettingsView(store: store.scope(state: \.tab2, action: \.tab2))
-                .tabItem {
-                    Image(systemName: "gear")
-                    Text("Settings")
-                }
+        WithPerceptionTracking {
+            TabView {
+                DisplayListView(store: store.scope(state: \.tab1, action: \.tab1))
+                    .tabItem {
+                        Image(systemName: "list.bullet")
+                        Text("Display")
+                    }
+                
+                SettingsView(store: store.scope(state: \.tab2, action: \.tab2))
+                    .tabItem {
+                        Image(systemName: "gear")
+                        Text("Settings")
+                    }
+            }
+            .preferredColorScheme(store.theme.colorScheme)            
         }
-        .preferredColorScheme(store.theme.colorScheme)
     }
 }
 
